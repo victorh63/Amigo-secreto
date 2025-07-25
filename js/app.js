c1b299fb541c57c263c8fd79a2f1b5bf410808ed
Victor <vichugo251106@hotmail.com>
2025-07-25 15:37:31 -0300

    Comentários (1)

--- js/app.js
+++ js/app.js
@@ -2,12 +2,15 @@ function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
     
+    // caso a lista esteja vazia, apenas adiciona o nome
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
+        // caso tenha algum conteúdo, mantém o texto anterior, adiciona , e espaço, e o novo nome  
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
-
+    
+    // limpeza do campo que preenche o nome no fim da função
     amigo.value = '';
 };
 
