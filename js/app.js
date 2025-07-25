8a2e81e6747b887de7b4fdd409b20784a7cf23b7
Victor <vichugo251106@hotmail.com>
2025-07-25 14:15:24 -0300

    Criação das functions 'adicionar', 'sortear' e 'reiniciar', e das lets 'amigo' e 'lista'

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+};
+
+function sortear() {};
+
+function reiniciar() {};
